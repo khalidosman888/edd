444f87bea6a39bfab0cfdb407da7cfe599b52476
add <romanosman67@gmail.com>
2023-09-18 06:32:07 +0300

    update

---
+++ edd.js
@@ -0,0 +1,17 @@
+let codes = document.getElementById("codes");
+let play = document.getElementById("play");
+let remove = document.getElementById("remove");
+let result = document.getElementById("result");
+
+play.onclick = ()=>{
+     result.innerHTML = codes.value;   
+     localStorage.setItem("RESULT" , codes.value);
+};
+remove.onclick = ()=>{
+    result.innerHTML = "";    
+};
+
+onload = ()=>{
+    codes.value =  localStorage.getItem("RESULT");
+    result.innerHTML = codes.value; 
+}
